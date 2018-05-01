d08a8c24c38224b463c12ded4b629a50d144dbce
ebiz <lustriaebiz@gmail.com>
2018-05-01 16:58:32 +0700

    welcome

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="welcome cotainer">
+  <div class="welcome container">
     <div class="card">
       <div class="card-content center-align">
         <h2 class="teal-text">Welcome</h2>
@@ -34,5 +34,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+.welcome{
+  max-width: 400px;
+  margin-top: 100px;
+}
 
+.welcome h2{
+  font-size: 3em;
+}
+
+.welcome button{
+  margin: 30px auto;
+}
 </style>
