0e3bfdbb8482fe022bb81798ec81692b3052a1a6
ebiz <lustriaebiz@gmail.com>
2018-05-01 17:32:42 +0700

    new message

---
+++ src/components/NewMessage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="new-message">
+      <form @submit.prevent="addMessage">
+          <label for="new-message"> New Message (enter to add): </label>
+          <input type="text" name="new-message" v-model="newMessage">
+      </form>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'NewMessage',
+        props: ['name'],
+        data(){
+            return{
+                newMessage: null
+            }
+        },
+        methods: {
+            addMessage(){
+                console.log('name: ', this.name);
+                console.log('message: ', this.newMessage);
+                console.log('date: ', Date.now());
+                
+                
+                 
+            }
+        }
+    }
+</script>
